d72f25a8d359215b90abb85c27409bcea5acdb63
ltp <1776494277@qq.com>
2017-03-27 10:12:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus_/percentlayoyt/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.asus_.percentlayoyt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
